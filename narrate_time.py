e0167b3e0cb3ded7166cd17dc11c796220b7bc3a
Sylvan Zheng <squidgetx@gmail.com>
2020-12-07 12:31:11 -0500

    more time narration

--- narrate_time.py
+++ narrate_time.py
@@ -1,8 +1,13 @@
 import tracery
 import datetime
+import util
+import random
 
 
-def narrate_month(date):
+def narrate_month(date, delta=random.random()):
+    # Delta is a value 0-1 indicating positiveness of the description
+    # 1 = very negative, 0 = very positive
+    delta = random.gauss(delta, 0.1)
     months = {
         1: 'winter',
         2: 'winter',
@@ -21,45 +26,58 @@ def narrate_month(date):
         'origin': f"\n{date.strftime('%B')} #brought# #{months[date.month]}#.\n",
         'brought': ['brought'],
         'winter': [
-            '#wadj# #wnoun# and #wadj# #wnoun#',
+            '#wadj# #wnoun# and #w#',
         ],
         'spring': [
-            '#sadj# #snoun# and #sadj# #snoun#',
+            '#sadj# #snoun# and #s#',
         ],
         'summer': [
-            '#suadj# #sunoun# and #suadj# #sunoun#',
+            '#suadj# #sunoun# and #su#',
         ],
         'fall': [
-            '#fadj# #fnoun# and #fadj# #fnoun#',
+            '#fadj# #fnoun# and #f#',
         ],
         'wadj': [
-            'crisp', 'harsh', 'cloudy', 'cold', 'crisp', 'snowy', 'grey', 'dark', 'wet', 'frosty'
+            'crisp', 'snowy', 'cloudy', 'grey', 'chilly', 'wet', 'frosty', 'blustery', 'drafty', 'cold', 'bleak', 'harsh', 'dark',
         ],
         'wnoun': [
             'days', 'mornings', 'evenings', 'winds', 'storms', 'breezes', 'blizzards', 'sunsets'
         ],
+        'w': [
+            'hot chocolate', 'warm blankets', 'wool flannels', '#wadj# #wnoun#', 'wet socks', 'frozen toes', 'fitful nights'
+        ],
         'sadj': [
-            'dewy', 'breezy', 'crisp', 'sunny', 'lush', 'young', 'delicate', 'balmy', 'vibrant', 'vivid'
+            'dewy', 'breezy', 'crisp', 'sunny', 'lush', 'young', 'delicate', 'balmy', 'vibrant', 'vivid', 'wet',
         ],
         'snoun': [
-            'breezes', 'days', 'mornings', 'evenings', 'flowers', 'sunrises', 'rains'
+            'breezes', 'days', 'mornings', 'evenings', 'sunrises', 'rains', 'showers'
+        ],
+        's': [
+            'blooming flowers', 'melodic bird calls', '#sadj# #snoun#', 'hay fever', 'allergies',
         ],
         'suadj': [
-            'warm', 'hot', 'sunny', 'stifling', 'humid', 'golden', 'muggy'
+            'golden', 'warm', 'hot', 'sunny', 'stifling', 'humid', 'searing', 'muggy', 'unbearably hot'
         ],
         'sunoun': [
-            'air', 'days', 'mornings', 'evenings', 'nights', 'sunrises'
+            'air', 'days', 'mornings', 'evenings', 'nights', 'sunrises', 'afternoons'
+        ],
+        'su': [
+            'beach days', 'barbeques', '#suadj# #sunoun#', 'cold air-conditioning', 'sweat',
         ],
         'fadj': [
-            'crisp', 'cool', 'gentle', 'beautiful', 'brisk', 'mild', 'warm', 'sunsets',
+            'crisp',  'gentle', 'beautiful', 'brisk', 'mild', 'warm', 'cool', 'frosty', 'bitter'
         ],
         'fnoun': [
-            'air', 'days', 'mornings', 'evenings', 'nights', 'leaves', 'foliage', 'breezes',
-        ]
+            'air', 'days', 'mornings', 'evenings', 'nights', 'leaves', 'foliage', 'breezes', 'sunsets',
+        ],
+        'f': [
+            'colorful leaves', 'warm sweaters', '#fadj# #fnoun#', 'tacky decorations', 'chapped lips',
+        ],
     }
     print(tracery.Grammar(rules).flatten('#origin#'))
 
 
 def narrate_time(last_event, event):
     if last_event and last_event['date'].month != event['date'].month:
-        narrate_month(event['date'])
+        narrate_month(event['date'], util.scale(
+            last_event['delta'] + event['delta'], -1, 1, 1, 0))
