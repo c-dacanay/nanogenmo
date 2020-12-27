c2f3a2b8b3dac2e7822f997ba8d587b95ac6aee5
Emily Zhao <emilyzhao@nyu.edu>
2020-12-27 12:36:46 -0500

    added utf-8 to fix emoji cursor

--- main.py
+++ main.py
@@ -89,6 +89,7 @@ def generate_book(num):
         print(f'''
             <html>
                 <head>
+                    <meta charset="UTF-8">
                     <link rel="stylesheet" type="text/css" href="style.css" />
                 </head>
                 <body>
