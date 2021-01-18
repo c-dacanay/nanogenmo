0ac35debd8d6b84720e962094ed11aeb07c241c5
Christina Dacanay <tina.dacanay@gmail.com>
2021-01-18 18:48:15 -0500

    move html to docs

--- make.sh
+++ make.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-mkdir -p html
+mkdir -p docs
 python3 main.py -n 100 > out
-python3 split_html.py < out
+python3 split_docs.py < out
 echo "100 chapters"
-echo "$(grep -r "<tt>meeting" html/ | wc -l) reached MEETING"
-echo "$(grep -r "<tt>courting" html/ | wc -l) reached COURTING"
-echo "$(grep -r "<tt>dating" html/ | wc -l) reached DATING"
-echo "$(grep -r "<tt>committed" html/ | wc -l) reached COMMITTED"
-echo "$(grep -rl "<tt>committed" html | sed 's/.*/            &/')"
+echo "$(grep -r "<tt>meeting" docs/ | wc -l) reached MEETING"
+echo "$(grep -r "<tt>courting" docs/ | wc -l) reached COURTING"
+echo "$(grep -r "<tt>dating" docs/ | wc -l) reached DATING"
+echo "$(grep -r "<tt>committed" docs/ | wc -l) reached COMMITTED"
+echo "$(grep -rl "<tt>committed" docs | sed 's/.*/            &/')"
--- split_html.py
+++ split_html.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     # Take string from STDIN and split it into multiple HTML documents
 
     index = 0
-    f = open(f'html/{index}.html', 'w')
+    f = open(f'docs/{index}.html', 'w')
     for line in sys.stdin:
         if '|' == line.rstrip():
             # Close the current file and begin writing the next one
